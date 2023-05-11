2178465aea6cb2d1dcddaf903fb96d29b282729d
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-05-11 05:10:45 +0300

    orders fix

--- src/pages/orders/index.js
+++ src/pages/orders/index.js
@@ -17,10 +17,90 @@ const Orders = () => {
   const dispatch = useDispatch();
   const { token } = useSelector((state) => state.user);
   const { orders, isLoading } = useSelector((state) => state.orders);
+  const [successTotal, setSuccessTotal] = useState(0);
+  const [failedTotat, setFailedTotal] = useState(0);
+  const [ordersToShow, setOrdersToshow] = useState([]);
+
+  //
+  const [deliveryStatusFilter, setDeliveryStatusFilter] = useState("");
+  const [paymentStatusFilter, setPaymentStatusFilter] = useState("");
+  const [dateFilter, setDateFilter] = useState("");
+  //
 
   useEffect(() => {
     dispatch(fetchOrders());
   }, []);
+
+  useEffect(() => {
+    let sub = true;
+    if (sub) {
+      const success = ordersToShow.filter(
+        (item) => item.paymentStatus === "SUCCESS"
+      );
+      const failed = ordersToShow.filter(
+        (item) => item.paymentStatus === "FAILED"
+      );
+
+      let s1 = 0;
+      let s2 = 0;
+      for (let i = 0; i < success.length; i++) {
+        s1 +=
+          Number(success[i].cartTotalAmount) + Number(success[i].deliveryFees);
+        // +
+        // Number(success[i].systemFees) +
+        // Number(success[i].packagingFees) +
+        // Number(success[i].agentFees);
+      }
+
+      for (let i = 0; i < failed.length; i++) {
+        s2 +=
+          Number(failed[i].cartTotalAmount) + Number(failed[i].deliveryFees);
+        // +
+        // Number(failed[i].systemFees) +
+        // Number(failed[i].packagingFees) +
+        // Number(failed[i].agentFees);
+      }
+      setSuccessTotal(s1);
+      setFailedTotal(s2);
+    }
+    return () => {
+      sub = false;
+    };
+  }, [ordersToShow]);
+
+  useEffect(() => {
+    let sub = true;
+    if (sub) {
+      let res = orders;
+      // if (keyWord.trim() !== "") {
+      //   res = allPcList.filter(
+      //     (item) =>
+      //       item.serialNumber.toLowerCase().includes(keyWord.toLowerCase()) ||
+      //       item.model.toLowerCase().includes(keyWord.toLowerCase())
+      //   );
+      // }
+      if (paymentStatusFilter !== "") {
+        res = res.filter((item) => item.paymentStatus === paymentStatusFilter);
+      }
+      if (deliveryStatusFilter !== "") {
+        res = res.filter(
+          (item) => item.deliveryStatus === deliveryStatusFilter
+        );
+      }
+
+      if (dateFilter !== "") {
+        res = res.filter(
+          (item) =>
+            new Date(item.createdAt).toLocaleDateString() ===
+            new Date(dateFilter).toLocaleDateString()
+        );
+      }
+      setOrdersToshow(res);
+    }
+    return () => {
+      sub = false;
+    };
+  }, [paymentStatusFilter, deliveryStatusFilter, dateFilter, orders]);
 
   return (
     <>
@@ -28,7 +108,56 @@ const Orders = () => {
         <Grid item xs={12} sm={12} md={12}>
           <Card>
             <Card.Header>
-              <strong>Orders List ({orders.length})</strong>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <strong>Orders List ({orders.length})</strong>
+                <div>
+                  <table>
+                    <tr>
+                      <td>
+                        <select
+                          value={deliveryStatusFilter}
+                          onChange={(e) =>
+                            setDeliveryStatusFilter(e.target.value)
+                          }
+                        >
+                          <option value="">Delivery Status</option>
+                          <option value="PENDING">PENDING</option>
+                          <option value="WAITING">WAITING</option>
+                          <option value="FAILED">FAILED</option>
+                          <option value="SUCCESS">SUCCESS</option>
+                          <option value="COMPLETED">COMPLETED</option>
+                        </select>
+                      </td>
+                      <td>
+                        <select
+                          value={paymentStatusFilter}
+                          onChange={(e) =>
+                            setPaymentStatusFilter(e.target.value)
+                          }
+                        >
+                          <option value="">Payment Status</option>
+                          <option value="PENDING">PENDING</option>
+                          <option value="SUCCESS">SUCCESS</option>
+                          <option value="FAILED">FAILED</option>
+                        </select>
+                      </td>
+                      <td>
+                        <input
+                          type="date"
+                          value={dateFilter}
+                          onChange={(e) => setDateFilter(e.target.value)}
+                        />
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+              </div>
             </Card.Header>
             <Card.Body>
               {isLoading ? (
@@ -54,7 +183,7 @@ const Orders = () => {
                       </tr>
                     </thead>
                     <tbody>
-                      {orders.map((item, index) => (
+                      {ordersToShow.map((item, index) => (
                         <tr>
                           <td>{item.id}</td>
                           <td>Market</td>
@@ -139,6 +268,14 @@ const Orders = () => {
                       ))}
                     </tbody>
                   </table>
+                  <p className="text-success">
+                    <b>Total success orders: </b>{" "}
+                    {currencyFormatter(successTotal)} RWF
+                  </p>
+                  <p className="text-danger">
+                    <b>Total failed orders: </b>{" "}
+                    {currencyFormatter(failedTotat)} RWF
+                  </p>
                 </div>
               )}
             </Card.Body>
