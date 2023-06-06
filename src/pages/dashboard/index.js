f81d784d61e6d686490a32759d2887cfa5378707
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-06-06 12:57:14 +0200

    dashboard fix

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -122,7 +122,6 @@ const DashboardDefault = () => {
 
   return (
     <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-      {/*       
       <Grid item xs={12} sx={{ mb: -2.25 }}>
         <Typography variant="h5">Dashboard</Typography>
       </Grid>
@@ -148,7 +147,7 @@ const DashboardDefault = () => {
           extra="/dashboard/transactions"
         />
       </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
+      {/* <Grid item xs={12} sm={6} md={4} lg={3}>
         <AnalyticEcommerce
           title="Supplier Payments"
           count={
@@ -157,14 +156,14 @@ const DashboardDefault = () => {
           color="warning"
           extra="/dashboard/supplierspayment"
         />
-      </Grid>
+      </Grid> */}
 
       <Grid
         item
         md={8}
         sx={{ display: { sm: "none", md: "block", lg: "none" } }}
       />
- 
+
       <Grid item xs={12} md={7} lg={8}>
         <Grid container alignItems="center" justifyContent="space-between">
           <Grid item>
@@ -231,17 +230,17 @@ const DashboardDefault = () => {
           </List>
         </MainCard>
       </Grid>
- 
+
       <Grid item xs={12} md={12} lg={12}>
         <Grid container alignItems="center" justifyContent="space-between">
           <Grid item>
             <Typography variant="h5">Sales Report</Typography>
-          </Grid> 
+          </Grid>
         </Grid>
-        <MainCard sx={{ mt: 1.75 }}> 
+        <MainCard sx={{ mt: 1.75 }}>
           <SalesColumnChart transactions={transactions} orders={orders} />
         </MainCard>
-      </Grid> */}
+      </Grid>
     </Grid>
   );
 };
